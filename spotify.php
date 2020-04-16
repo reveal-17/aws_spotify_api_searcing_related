004b8d111c2c69754723d3ac55fbb5b5cd56f4d9
宮崎脩平 <miyazakishuhei@miyazakiosamutairanoMacBook-Pro.local>
2020-04-16 13:22:58 +0900

    環境変数ライブラリ追加

--- spotify.php
+++ spotify.php
@@ -1,9 +1,10 @@
 <?php
 require_once('vendor/autoload.php');
 
-// 環境変数ライブラリ
-$dotenv = new Dotenv\Dotenv(__DIR__);
+// 環境変数使用ライブラリ
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
 $CLIENT_ID = getenv('CLIENT_ID');
 $CLIENT_SECRET = getenv('CLIENT_SECRET');
 
